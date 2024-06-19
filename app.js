f97c282d8dcb89ce2b0e68f1179e6e310d886974
denishraiyani <denish.raiyani96@gmail.com>
2024-06-19 12:44:04 +0530

    Route: Create New-Folder

--- app.js
+++ app.js
@@ -10,9 +10,23 @@ app.use(fileupload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Root Route
+// Route: Root
 app.get("/", (req, res) => {
   res.send(`I am Root Route.`);
+});
+
+// Route: Create New-Folder
+app.post("/create-folder", (req, res) => {
+  // console.log(req.body);
+  const folderName = req.body.folderName;
+  const folderPath = path.join(__dirname, "uploads", folderName);
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+    res.status(200).send(`Folder "${folderName}" created successfully.`);
+  } else {
+    res.status(400).send(`Folder "${folderName}" already exist.`);
+  }
 });
 
 const PORT = 8080;
