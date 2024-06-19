a632e2b788fe480c614ea99ae6fa60ed458a6dc3
denishraiyani <denish.raiyani96@gmail.com>
2024-06-19 13:07:15 +0530

    Route: Upload File

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const path = require("path");
 const app = express();
 
 // Middleware
-app.use(fileupload());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(fileupload());
 
 // Route: Root
 app.get("/", (req, res) => {
@@ -27,6 +27,32 @@ app.post("/create-folder", (req, res) => {
   } else {
     res.status(400).send(`Folder "${folderName}" already exist.`);
   }
+});
+
+// Route: File Upload
+app.post("/upload-file", (req, res) => {
+  console.log(req.body);
+  console.log(req.files);
+
+  const folderName = req.body.folderName;
+  const uploadedFile = req.files.file;
+
+  const fileName = uploadedFile.name;
+  const fileBufferData = uploadedFile.data;
+
+  const folderPath = path.join(__dirname, "uploads", folderName);
+  const filePath = path.join(__dirname, "uploads", folderName, fileName);
+
+  if (!fs.existsSync(folderPath)) {
+    fs.mkdirSync(folderPath, { recursive: true });
+  }
+
+  fs.writeFile(filePath, fileBufferData, (err) => {
+    if (err) {
+      return res.status(500).send(`File not saved.`);
+    }
+    res.send(`file saved to ${filePath}`);
+  });
 });
 
 const PORT = 8080;
